6a8625e0699f79ad4b02090076fa64913e818521
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-19 21:43:10 +0600

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { CustomChartComponent } from "@/components/chart-component";
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen flex flex-col justify-center">
+      <CustomChartComponent />
+    </main>
+  );
+}
