504221d59476a1e4408961e0fecbf7118e97a45f
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-19 21:43:40 +0600

    init

---
+++ src/config/site.config.ts
@@ -0,0 +1,28 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Recharts Gantt Chart",
+  title: "Recharts Gantt Chart (ShadCN chart) with Zooming and Brush",
+  description:
+    "A simple example of a Gantt chart using Recharts with zooming and brush.",
+  keywords: [
+    "shadcn",
+    "gantt",
+    "gantt chart",
+    "chart",
+    "brush",
+    "zoom",
+    "typescript",
+    "react",
+  ],
+  creator: {
+    name: "rds_agi",
+    url: "https://rdsx.dev",
+  },
+  siteUrl: "https://recharts-gantt-chart.rdsx.dev",
+  ogImage: "https://recharts-gantt-chart.rdsx.dev/og.png",
+  links: {
+    x: "https://x.com/rds_agi",
+    github: "https://github.com/rudrodip/recharts-gantt-chart",
+  },
+};
